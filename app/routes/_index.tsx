7788a24cc2a9e25bd68b7263d457d8cdaabdc99e
Yohannes-Maye <166595409+Yohannes-Maye-01@users.noreply.github.com>
2025-03-20 08:48:45 +0300

    car

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,7 +14,17 @@ export default function Index() {
 
     </div>
     
-      
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        
+      </body>
+      </html>
    
 
 
