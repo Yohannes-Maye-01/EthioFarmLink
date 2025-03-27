1988661b529a3d61c1473d24a3f207f2d25e59cd
Yohannes-Maye <166595409+Yohannes-Maye-01@users.noreply.github.com>
2025-03-27 17:59:03 +0300

    Update Main.tsx

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -32,6 +32,24 @@ export default function Main() {
           <li>
             <Link to="/about">About</Link>
           </li>
+           <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+           <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+           <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </nav>
     </div>
