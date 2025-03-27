c57e6ee3d3e6c732f693767575dbe96f42bf8366
Yohannes-Maye <166595409+Yohannes-Maye-01@users.noreply.github.com>
2025-03-27 17:53:56 +0300

    Create Main.tsx

---
+++ app/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main className="p-4 bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Welcome to Main Component</h1>
+    </main>
+  );
+};
+
+export default Main;
